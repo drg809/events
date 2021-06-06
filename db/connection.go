4b34ab5eb42a02cd6472a87380932fbfd84deaa7
Daniel Rodriguez <drg809@gmail.com>
2021-06-06 23:43:38 +0200

    subido archivo de connexion

---
+++ db/connection.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoCN = ConnectDB()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://dani:#xs9xQcDtGP2rwvr1@cluster0.1pxzp.mongodb.net/events?retryWrites=true&w=majority")
+
+func ConnectDB() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Println("Conexión exitosa con la BD")
+	return client
+}
+
+func CheckConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
