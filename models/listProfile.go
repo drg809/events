58db9d30a306d574fc6d9651c98e0055edea419c
Daniel Rodriguez <drg809@gmail.com>
2021-06-25 23:50:57 +0200

    add date filed to list profile

--- models/listProfile.go
+++ models/listProfile.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -8,7 +10,7 @@ type ListProfile struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Name     string             `bson:"name" json:"name,omitempty"`
 	Surname  string             `bson:"surname" json:"surname,omitempty"`
-	Email    string             `bson:"email" json:"email"`
+	Date     time.Time          `bson:"date" json:"date,omitempty"`
 	Avatar   string             `bson:"avatar" json:"avatar,omitempty"`
 	Banner   string             `bson:"banner" json:"banner,omitempty"`
 	Bio      string             `bson:"bio" json:"bio,omitempty"`
