04be4e0dcee800ec2a812c9ce04df3f4b78b7b1b
Daniel Rodriguez <drg809@gmail.com>
2021-06-25 23:41:24 +0200

    add new fields in listprofile

--- models/listProfile.go
+++ models/listProfile.go
@@ -5,11 +5,13 @@ import (
 )
 
 type ListProfile struct {
-	ID      primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Name    string             `bson:"name" json:"name,omitempty"`
-	Surname string             `bson:"surname" json:"surname,omitempty"`
-	Email   string             `bson:"email" json:"email"`
-	Avatar  string             `bson:"avatar" json:"avatar,omitempty"`
-	Banner  string             `bson:"banner" json:"banner,omitempty"`
-	Bio     string             `bson:"bio" json:"bio,omitempty"`
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name     string             `bson:"name" json:"name,omitempty"`
+	Surname  string             `bson:"surname" json:"surname,omitempty"`
+	Email    string             `bson:"email" json:"email"`
+	Avatar   string             `bson:"avatar" json:"avatar,omitempty"`
+	Banner   string             `bson:"banner" json:"banner,omitempty"`
+	Bio      string             `bson:"bio" json:"bio,omitempty"`
+	Location string             `bson:"location" json:"location,omitempty"`
+	Web      string             `bson:"web" json:"web,omitempty"`
 }
