ffe4be3997bbc4dbec9497f070df6f1971021c06
Daniel Rodriguez <drg809@gmail.com>
2021-07-04 19:39:17 +0200

    id in model

--- models/event.go
+++ models/event.go
@@ -1,12 +1,17 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Event struct {
-	UserID string    `bson:"userId" json:"userId,omitempty"`
-	Name   string    `bson:"name" json:"name"`
-	Detail string    `bson:"detail" json:"detail"`
-	Date   time.Time `bson:"date" json:"date,omitempty"`
-	Type   bool      `bson:"type" json:"type,omitempty"`
-	Photo  string    `bson:"photo" json:"photo,omitempty"`
+	ID     primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
+	UserID string             `bson:"userId" json:"userId,omitempty"`
+	Name   string             `bson:"name" json:"name"`
+	Detail string             `bson:"detail" json:"detail"`
+	Date   time.Time          `bson:"date" json:"date,omitempty"`
+	Type   bool               `bson:"type" json:"type,omitempty"`
+	Photo  string             `bson:"photo" json:"photo,omitempty"`
 }
